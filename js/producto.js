05a85a399cbffd8f5d4628a61382d1ea431fb196
Sabrina Micaela Sosa <126528316+aveespacial@users.noreply.github.com>
2023-10-02 20:40:25 -0300

    Update producto.js
    función reemplazar estrellas

--- js/producto.js
+++ js/producto.js
@@ -1,7 +1,20 @@
 const destacado = JSON.parse(localStorage.getItem("producto"));
 
 
-const mostrarProducto = document.querySelector("#producto-elegido");
+const mostrarProducto = document.querySelector("#producto-elegido"); 
+
+const p = document.createElement("p");
+
+
+function convertirAEstrellas(Puntuación) {
+    const estrellasLlenas = Puntuación.length;
+    const estrellasVacias = 5 - estrellasLlenas;
+
+    const estrellasHTML = '<span class="estrella-llena">★</span>'.repeat(estrellasLlenas) +
+        '<span class="estrella-vacia">☆</span>'.repeat(estrellasVacias);
+
+    return `<p class="puntuacion-estrellas">${estrellasHTML}</p>`;
+}
  
 destacado.forEach((product) => {
     let content = document.createElement("div");
@@ -15,7 +28,7 @@ destacado.forEach((product) => {
         </div>
         <div>
             <p class="price-product">${product.precio}</p>
-            <p class="price-product">${product.puntaje}</p>
+           <p>${convertirAEstrellas(product.Puntuación)}</p> 
         </div>
     `;
 
